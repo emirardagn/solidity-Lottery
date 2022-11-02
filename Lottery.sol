a2e57d8b9ccf2cd493bf7aefea1398f6e675fd8e
emirardagn <arda.gun.24752@ozu.edu.tr>
2022-11-02 21:00:32 +0300

    new file:   Lottery.sol

---
+++ Lottery.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.17;
+
+contract Lottery{
+    address public manager;
+    address[] public players;
+
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable{
+        //require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+
+
+
+    function random() private returns(uint){
+        return uint (keccak256(players,block.timestamp,block.difficulty));
+    }
+
+    function pickWinner() public {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+    }
+
+}
